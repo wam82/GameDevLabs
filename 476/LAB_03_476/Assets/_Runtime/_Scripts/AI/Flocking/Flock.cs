cfadc3c5e73a22d3b0de54e42862d473bdc3359c
wam82 <monsterwam8282@gmail.com>
2025-02-05 18:12:18 -0500

    Lab 3 of 476

---
+++ 476/LAB_03_476/Assets/_Runtime/_Scripts/AI/Flocking/Flock.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+public class Flock : MonoBehaviour 
+{
+    public bool debug;
+    public int startingFlockCount = 20;
+    public GameObject flockAgentPrefab;
+    public float neighborRadius = 5;
+    public float avoidanceRadius = 3.5f;
+    public float cohesionFactor = 1.5f;
+    public float avoidanceFactor = 2f;
+    public float seekSpeed = 3f;
+
+    private List<FlockAgent> swarm = new List<FlockAgent>();
+    private Queue<Transform> targetList = new Queue<Transform>();
+
+    private void Start()
+    {
+        foreach (GameObject targetObj in GameObject.FindGameObjectsWithTag("Target"))
+            targetList.Enqueue(targetObj.transform);
+
+        GenerateSwarm();
+        SetNewSwarmTarget();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            SetNewSwarmTarget();
+
+        if (Input.GetKey(KeyCode.Q))
+            cohesionFactor += Time.deltaTime;
+        else if (Input.GetKey(KeyCode.A))
+            cohesionFactor -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.W))
+            avoidanceFactor += Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            avoidanceFactor -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.E))
+            seekSpeed += Time.deltaTime;
+        else if (Input.GetKey(KeyCode.D))
+            seekSpeed -= Time.deltaTime;
+
+        // synchronize swarm settings across all agents
+        foreach (FlockAgent agent in swarm)
+        {
+            agent.debug = debug;
+            agent.neighborRadius = neighborRadius;
+            agent.avoidanceRadius = avoidanceRadius;
+            agent.cohesionFactor = cohesionFactor;
+            agent.avoidanceFactor = avoidanceFactor;
+            agent.seekSpeed = seekSpeed;
+        }
+    }
+
+    private void SetNewSwarmTarget()
+    {
+        // Managing the Queue. We grab the target, set the swarmlings on it, then requeue it at the back.
+        Transform target = targetList.Dequeue();
+        foreach (FlockAgent agent in swarm)
+        {
+            agent.target = target;
+        }
+        targetList.Enqueue(target);
+    }
+
+    private void GenerateSwarm()
+    {
+        swarm.Clear();
+        const float AGENT_DENSITY = 0.08f;
+        for (int i = 0; i < startingFlockCount; ++i)
+        {
+            FlockAgent agent = Instantiate(
+                flockAgentPrefab, transform.position + (Random.insideUnitSphere * startingFlockCount * AGENT_DENSITY), 
+                Quaternion.identity, 
+                transform
+            ).GetComponent<FlockAgent>();
+
+            if (agent != null)
+                swarm.Add(agent);
+        }
+    }
+}
---
+++ 476/LAB_03_476/Assets/_Runtime/_Scripts/AI/Flocking/FlockAgent.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+using Utilities;
+
+public class FlockAgent : MonoBehaviour 
+{
+    public bool debug;
+    public Transform target;
+    public float neighborRadius = 5;
+    public float avoidanceRadius = 3.5f;
+    public float cohesionFactor = 1.5f;
+    public float avoidanceFactor = 2f;
+    public float seekSpeed = 3f;
+
+    private Vector3 movement;
+	
+	void Update () 
+    {
+        Collider[] neighbors = GetNeighborContext();
+        Cohesion(neighbors);
+        Avoidance(neighbors);
+        Alignment(neighbors);
+        Seek();
+
+        // TODO : steer and align the boid in the direction of the movement
+		
+    }
+
+    public Collider[] GetNeighborContext()
+    {
+        Collider[] neighbors = Physics.OverlapSphere(transform.position, neighborRadius);
+
+        if (debug)
+            DebugUtil.DrawWireSphere(transform.position, Color.Lerp(Color.white, Color.red, neighbors.Length), neighborRadius);
+
+        return neighbors;
+    }
+
+    // This is the force that keeps the swarm together.
+    void Cohesion(Collider[] neighbors)
+    {
+        // movement is equal to the relative offset from the flock agent to the center of the flock
+        // TODO
+		
+    }
+
+    // This is the force that dictates the spacing of the swarm.
+    void Avoidance(Collider[] neighbors)
+    {
+        // movement is equal to the average of the sum of all vectors going from neighbor to flock agent within the avoidance radius
+        // TODO
+		
+    }
+
+    // This "force" has each of the agents try to synch their orientation.
+    void Alignment(Collider[] neighbors)
+    {
+        // alignedDirection is equal to the average direction of neighbors 
+        // TODO
+		
+    }
+
+    // This has each agent try to seek out its current target. It's possible to do this
+    // in the FlockMind as well, but this is more of a "distributed" swarm model, so each agent
+    // gets to make its decisions locally.
+    void Seek()
+    {
+        // TODO
+		
+    }
+}
